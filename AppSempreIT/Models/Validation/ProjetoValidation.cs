bd3e8cc6751929e31d8e5022ce97196de6041c92
Ariel Veras <ariolvs123@gmail.com>
2022-07-22 09:50:24 -0400

    [tarefa] campos de datas obrigatórios.

--- AppSempreIT/Models/Validation/ProjetoValidation.cs
+++ AppSempreIT/Models/Validation/ProjetoValidation.cs
@@ -8,7 +8,9 @@ namespace AppSempreIT.Models.Validation
         public ProjetoValidation()
         {
             RuleFor(x => x.NomeDoProjeto).NotNull().MinimumLength(3).WithMessage("Informe um nome de projeto com a partir de 3 caracteres no mínimo.");
-            RuleFor(x => x.ResponsavelPeloProjeto).NotNull().WithMessage("É obrigatório informar o responsável do projeto");
+            RuleFor(x => x.ResponsavelPeloProjeto).NotNull().WithMessage("É obrigatório informar o responsável do projeto.");
+            RuleFor(x => x.DataDeInicio).NotEmpty().WithMessage("É obrigatório informar a data de início.");
+            RuleFor(x => x.DataDeConclusao).NotEmpty().WithMessage("É obrigatório informar a data de conclusão.");
         }
     }
 }
