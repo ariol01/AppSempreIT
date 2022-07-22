44e4d3b6fa82e0d2899e10ddf7210e6ed727141c
Ariel Veras <ariolvs123@gmail.com>
2022-07-22 11:11:39 -0400

    [tarefa] subindo migration  e conexao com banco em nuvem.

---
+++ AppSempreIT/Migrations/20220722150816_FirstMigartion.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppSempreIT.Migrations
+{
+    public partial class FirstMigartion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projetos",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NomeDoProjeto = table.Column<string>(nullable: false),
+                    ResponsavelPeloProjeto = table.Column<string>(nullable: false),
+                    DataDeInicio = table.Column<DateTime>(nullable: false),
+                    DataDeConclusao = table.Column<DateTime>(nullable: false),
+                    DataDeCriacaoDoRegistros = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projetos", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Projetos");
+        }
+    }
+}
